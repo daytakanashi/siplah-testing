126fc792c6631eab954b55139749e79a03a84227
daytakanashi <48751922+daytakanashi@users.noreply.github.com>
2020-02-13 11:05:04 +0700

    bersih bersih

--- cypress/integration/penjualBaru.spec.js
+++ cypress/integration/penjualBaru.spec.js
@@ -23,16 +23,42 @@ context('Actions', () => {
 
          // login penjual
          cy
-         .wait(10000)
          .get('#btn_masuk')
          .click()
          .get('#usernameLoginRef')
-         .type('takanashi@gmail.com')
-         .wait(1000)
+         .click()
+         .type('takanashiday@gmail.com')
          .get('.product-content > .mt15 > .grid-x > .large-12 > :nth-child(3) > .ng-untouched')
+         .click()
          .type('123456789')
          .get('#reveal-masuk > .product-content > .mt15 > .grid-x > .large-12 > .button')
          .click()
+
+        //  pengaturan akun
+        cy
+        .get('.rounded-img')
+        .click()
+        .get('[tabindex="0"] > .f-black')
+        .click()
+        cy.get('.grid-x > :nth-child(1) > .logo-dobuy > .mr-auto')
+        .click()
+        .get('.rounded-img')
+        .click()
+        .get('[tabindex="0"] > .f-black')
+        .click()
+        // nama toko
+        .get('#namaToko-input')
+        .click()
+        .type('toko coba 1')
+        // nama domain
+        .get('#namaDomain-input')
+        .click()
+        .type('tokoCoba')
+        // pilih kategori
+        .get('#namaCategory-input')
+        .click()
+        .type('buku')
+        // .select('buku')
 
     })
 })
